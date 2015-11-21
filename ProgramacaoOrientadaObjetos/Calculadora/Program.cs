70a7456b4cc02ad27174141579626f883faf49ed
Rodrigo Prandi Araújo <rodrigo.prandi@outlook.com>
2015-11-21 14:06:25 -0200

    Calculadora

---
+++ ProgramacaoOrientadaObjetos/Calculadora/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Resultado: " + Somar(1, 3));
+            Console.WriteLine("Resultado: " + Subtrair(200, 52));
+            Console.WriteLine("Resultado: " + Multiplicar(23, 13));
+            Console.WriteLine("Resultado: " + Dividir(3455, 3));
+
+            Console.WriteLine("Resultado: " + Somar("Rodrigo", "Prandi"));
+
+            Console.ReadKey();
+        }
+
+        //public static void Imprimir(Action<int,int> operacao, int valor1, int valor2)
+        //{
+        //    Console.WriteLine("Resultado: " + operacao.Invoke(valor1, valor2));
+        //}
+
+        public static int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static string Somar(string a, string b)
+        {
+            return a + " " + b;
+        }
+
+        public static int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Dividir(int dividendo, int divisor)
+        {
+            return dividendo / divisor;
+        }
+    }
+}
